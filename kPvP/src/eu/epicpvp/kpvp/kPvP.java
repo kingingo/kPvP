128a395c4bfeb12c7a99b616d42b5cc6d96e137e
kingingo <kingingohd@googlemail.com>
2016-10-18 21:58:27 +0200

    add ce

--- kPvP/src/eu/epicpvp/kpvp/kPvP.java
+++ kPvP/src/eu/epicpvp/kpvp/kPvP.java
@@ -28,9 +28,16 @@ import eu.epicpvp.kcore.deliverychest.DeliveryChest;
 import eu.epicpvp.kcore.deliverychest.ItemModifier;
 import eu.epicpvp.kcore.enchantment.AnvilEnchantHandler;
 import eu.epicpvp.kcore.enchantment.CustomEnchantment;
+import eu.epicpvp.kcore.enchantment.enchantments.armor.FrozenEnchantmentListener;
+import eu.epicpvp.kcore.enchantment.enchantments.armor.ObsidianshieldEnchantmentListener;
+import eu.epicpvp.kcore.enchantment.enchantments.armor.PoisenedEnchantmentListener;
 import eu.epicpvp.kcore.enchantment.enchantments.attack.BlindEnchantmentListener;
 import eu.epicpvp.kcore.enchantment.enchantments.attack.LifestealEnchantmentListener;
 import eu.epicpvp.kcore.enchantment.enchantments.attack.PoisonEnchantmentListener;
+import eu.epicpvp.kcore.enchantment.enchantments.attack.WitherEnchantmentListener;
+import eu.epicpvp.kcore.enchantment.enchantments.bow.BlazeEnchantmentListener;
+import eu.epicpvp.kcore.enchantment.enchantments.bow.LightningEnchantmentListener;
+import eu.epicpvp.kcore.enchantment.enchantments.tools.SmeltingEnchantmentListener;
 import eu.epicpvp.kpvp.Listener.Listener;
 import lombok.Getter;
 
@@ -86,6 +93,14 @@ public class kPvP extends JavaPlugin {
 			new CustomEnchantment("Blind", new BlindEnchantmentListener());
 			new CustomEnchantment("Lifesteal", new LifestealEnchantmentListener());
 			new CustomEnchantment("Poison", new PoisonEnchantmentListener());
+			new CustomEnchantment("Smelting", new SmeltingEnchantmentListener());
+			new CustomEnchantment("Lightning", new LightningEnchantmentListener(5));
+			new CustomEnchantment("Blaze", new BlazeEnchantmentListener());
+			new CustomEnchantment("Wither", new WitherEnchantmentListener());
+			new CustomEnchantment("Poison", new PoisonEnchantmentListener());
+			new CustomEnchantment("Poisened", new PoisenedEnchantmentListener());
+			new CustomEnchantment("Frozen", new FrozenEnchantmentListener());
+			new CustomEnchantment("Obsidianshield", new ObsidianshieldEnchantmentListener());
 			
 			new DeliveryChest(this, UtilServer.getUserData(), new ItemModifier() {
 				
